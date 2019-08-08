b6f73d1b8e71dca8be2bd517bdf82616356b24f1
David Bae <DavidBae@Davids-MacBook-Pro-2.local>
2019-08-08 18:35:51 -0500

    update json

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { Stack } = require('./ADT/stack');
 const { MaxHeap } = require('./Trees/maxHeap');
 const { MinHeap } = require('./Trees/minHeap');
 
-const list = new LinkedList();
-console.log(list);
-const que = new Queue();
-console.log(que)
-const stack = new Stack();
-console.log(stack);
-const max = new MaxHeap();
-console.log(max);
-const min = new MinHeap();
-console.log(min);
+// const list = new LinkedList();
+// console.log(list);
+// const que = new Queue();
+// console.log(que)
+// const stack = new Stack();
+// console.log(stack);
+// const max = new MaxHeap();
+// console.log(max);
+// const min = new MinHeap();
+// console.log(min);
