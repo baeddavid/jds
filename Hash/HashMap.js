0d4abfd104bae49cdfcf90386a560323e0b7ca28
David Bae <DavidBae@Davids-MacBook-Pro-2.local>
2019-08-09 17:13:08 -0500

    add hashmap

--- Hash/HashMap.js
+++ Hash/HashMap.js
@@ -1,18 +1,48 @@
-// const LinkedList = require('../lists/linkedlist');
+const Hash = require('./Hash');
 
-// class HashMap {
-//     constructor() {
-//         this.hashArr = [];
-//         this.length = 0;
-//     }
+class HashMap extends Hash {
+    constructor() {
+        this.hashArr = [];
+        this.length = 0;
+    }
 
+    put(key, value) {
+        let idx = this.hash(key);
+        if(this.containsKey(key))
+            this.hashArr[idx] = this.hashArr[idx]++;
+        else
+            this.hashArr[idx] = value;
+        return true;
+    }
 
+    remove(key) {
+        let idx = this.hash(key);
+        let returnValue = this.hashArr[idx];
+        this.hashArr[idx] = null;
+        return returnValue;
+    }
 
-//     size() {
-//         return this.length;
-//     }
+    containsValue(value) {
+        for(let index of this.hashArr)
+            if(x == value)
+                return true;
+        return false;
+    }
 
-//     isEmpty() {
-//         return this.length == 0;
-//     }
-//}
+    containsKey(key) {
+        let idx = this.hash(key);
+        if(!!this.hashArr[idx])
+            return true;
+        return false;
+    }
+
+    size() {
+        return this.length;
+    }
+
+    isEmpty() {
+        return this.length == 0;
+    }
+}
+
+module.exports = HashMap;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const LinkedList = require('./lists/linkedlist');
 const Queue= require('./ADT/queue');
 const Stack = require('./ADT/stack');
+const LinkedStack = require('./ADT/LinkedStack');
+const LinkedQueue = require('./ADT/LinkedQueue');
 const MaxHeap = require('./Trees/maxHeap');
 const MinHeap = require('./Trees/minHeap');
 const HashSet = require('./Hash/HashSet');
-const LinkedStack = require('./ADT/LinkedStack');
-const LinkedQueue = require('./ADT/LinkedQueue');
+const HashMap = require('./Hash/HashMap');
 
-module.exports = {LinkedList, Queue, Stack, MaxHeap, MinHeap, HashSet, LinkedStack, LinkedQueue};
+module.exports = {LinkedList, Queue, Stack, MaxHeap, MinHeap, HashSet, LinkedStack, LinkedQueue, HashMap};
