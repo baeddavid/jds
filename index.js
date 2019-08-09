1509ced228b9fa39e4542fd744f2afd4560664aa
David Bae <DavidBae@Davids-MacBook-Pro-2.local>
2019-08-08 20:42:32 -0500

    fix console log

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const Queue= require('./ADT/queue');
 const Stack = require('./ADT/stack');
 const MaxHeap = require('./Trees/maxHeap');
 const MinHeap = require('./Trees/minHeap');
-let max = new MaxHeap();
-max.insert(4);
+
 module.exports = {LinkedList, Queue, Stack, MaxHeap, MinHeap};
