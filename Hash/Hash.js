e0c5b733380f7d66aed24485462583bea48a4683
David Bae <baeddavid@gmail.com>
2019-08-12 12:29:07 -0500

    update hash function

--- Hash/Hash.js
+++ Hash/Hash.js
@@ -1,8 +1,19 @@
+/**
+ * Credit to https://stackoverflow.com/questions/8511281/check-if-a-value-is-an-object-in-javascript
+ * and https://stackoverflow.com/questions/194846/is-there-any-kind-of-hash-code-function-in-javascript
+ * for helping to provide hashing and object hashing function
+ */
+
+const isObj = Symbol('isObj');
+
 class Hash {
     constructor() { } 
 
-    hash(key) {
-        key = key.toString();
+     hash(key) {
+        if(this[isObj](key))
+            key = JSON.stringify(key)
+        else
+            key = key.toString();
         let hash = 0;
         if (key.length == 0) {
             return hash;
@@ -12,8 +23,13 @@ class Hash {
             hash = ((hash<<5)-hash)+char;
             hash = hash & hash; // Convert to 32bit integer
         }
-        return Math.abs(hash - 100000);
+        return Math.abs(hash);
     }
+
+    [isObj](obj) {
+        let type = typeof obj;
+        return type === 'function' || type === 'object' && !!obj;
+      }
 }
 
 module.exports = Hash;
